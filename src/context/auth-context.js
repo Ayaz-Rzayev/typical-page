047962a82612d50b150519a6fb3889a448a3d857
Ayaz-Rzayev <rzayev.ayaz198@gmail.com>
2022-02-06 21:43:46 +0400

    Login and logout logic moved to auth-context.js

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
-  onLogout: ()=>{}
+  onLogout: ()=>{},
+  onLogin: (email, password) => {}
 })
+
+
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    const loggedInInfo = localStorage.getItem("isLoggedIn");
+    console.log("logged");
+    if (loggedInInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true)
+  }
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false)
+  }
+
+  return <AuthContext.Provider value ={{isLoggedIn: isLoggedIn, onLogout: logoutHandler, onLogin: loginHandler}}>
+    {props.children}
+  </AuthContext.Provider>
+}
 
 export default AuthContext
